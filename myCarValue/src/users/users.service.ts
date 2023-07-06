ee7c71455065c0cdeb509cfb24964d6841ee400b
kelvincheung000 <cheungkamkit@gmail.com>
2023-07-06 23:31:03 +0800

    added preventing access with authenticaiton guards

--- myCarValue/src/users/users.service.ts
+++ myCarValue/src/users/users.service.ts
@@ -14,12 +14,13 @@ export class UsersService {
     }
 
     findOne(id: number) {
-        // return this.repo.findOne(id);
+        if (!id) {
+            return null;
+        }
         return this.repo.findOne({ where: { id } });
     }
 
     find(email: string) {
-        // return this.repo.find({ email });
         return this.repo.find({ where: { email } });
 
     }
